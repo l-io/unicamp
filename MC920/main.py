09ab572a9be625800b744c8b5ce6f300a31abb46
Leonardo Rodrigues <lrm5060@gmail.com>
2022-09-05 22:39:58 -0300

    Task 1.1-b complete

--- MC920/main.py
+++ MC920/main.py
@@ -1,8 +1,11 @@
+from threading import local
+from time import process_time_ns
 import cv2
 import os
 
 # Get absolute path to images directory
-absolute_path = os.path.join(os.path.abspath(os.getcwd()), "images")
+local_path = os.path.abspath(os.getcwd())
+absolute_path = os.path.join(local_path, "images")
 
 # Create paths to images
 images_paths = []
@@ -15,4 +18,19 @@ for file in files:
 images = []
 
 for path in images_paths:
-    images.append(cv2.imread(path))
+    images.append(cv2.imread(path, cv2.IMREAD_GRAYSCALE))
+
+# 1.1
+
+# 1.1-b
+neg_images = []
+
+# apply negative
+for image in images:
+    neg_images.append(255 - image)
+
+# save in directory 11b
+for i in range(0, len(neg_images)):
+    cv2.imwrite(os.path.join(local_path, '11b', files[i]), neg_images[i])
+
+# 1.1-c
