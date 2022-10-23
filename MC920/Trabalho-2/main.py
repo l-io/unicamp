57dbfa09825ae5dddac3048d22fc588337e565ec
Leonardo Rodrigues <lrm5060@gmail.com>
2022-10-23 15:37:08 -0300

    Add report

--- MC920/Trabalho-2/main.py
+++ MC920/Trabalho-2/main.py
@@ -1,8 +1,11 @@
 
+from time import process_time_ns
+from turtle import width
 import cv2
 import os
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
 
 from load_images import load_images
 
@@ -19,7 +22,7 @@ def global_method(src_image, name, path):
     cv2.imwrite(os.path.join(path, 'output', 'global',
                 "{}-{}".format('global', name)), image)
 
-    print('Global Method applied to {}.'. format(name))
+    count_blackpixels(image, name, 'Global')
 
 
 def bersen_method(src_image, name, path):
@@ -37,7 +40,7 @@ def bersen_method(src_image, name, path):
     cv2.imwrite(os.path.join(path, 'output', 'bersen',
                 "{}-{}".format('bersen', name)), image)
 
-    print('Bersen Method applied to {}.'. format(name))
+    count_blackpixels(image, name, 'Bersen')
 
 
 def niblack_method(src_image, name, path):
@@ -55,7 +58,7 @@ def niblack_method(src_image, name, path):
     cv2.imwrite(os.path.join(path, 'output', 'niblack',
                 "{}-{}".format('niblack', name)), image)
 
-    print('Niblack Method applied to {}.'. format(name))
+    count_blackpixels(image, name, 'Niblack')
 
 
 def sp_method(src_image, name, path):
@@ -75,7 +78,7 @@ def sp_method(src_image, name, path):
     cv2.imwrite(os.path.join(path, 'output', 'sp',
                 "{}-{}".format('sp', name)), image)
 
-    print('SP Method applied to {}.'. format(name))
+    count_blackpixels(image, name, 'SP')
 
 
 def phansalskar_method(src_image, name, path):
@@ -97,7 +100,7 @@ def phansalskar_method(src_image, name, path):
     cv2.imwrite(os.path.join(path, 'output', 'phns',
                 "{}-{}".format('phns', name)), image)
 
-    print('PHNS Method applied to {}.'. format(name))
+    count_blackpixels(image, name, 'PHNS')
 
 
 def constrast_method(src_image, name, path):
@@ -115,7 +118,7 @@ def constrast_method(src_image, name, path):
     cv2.imwrite(os.path.join(path, 'output', 'contrast',
                 "{}-{}".format('contrast', name)), image)
 
-    print('Constrast Method applied to {}.'. format(name))
+    count_blackpixels(image, name, 'Contrast')
 
 
 def mean_method(src_image, name, path):
@@ -130,9 +133,9 @@ def mean_method(src_image, name, path):
             image[y][x] = 255 if src_image[y][x] > threshold else 0
 
     cv2.imwrite(os.path.join(path, 'output', 'mean',
-                "{}-{}".format('mean', name)), image)
+                "{}-{}".format('mean', name)), image[0:])
 
-    print('Mean Method applied to {}.'. format(name))
+    count_blackpixels(image, name, 'Mean')
 
 
 def median_method(src_image, name, path):
@@ -149,7 +152,7 @@ def median_method(src_image, name, path):
     cv2.imwrite(os.path.join(path, 'output', 'median',
                 "{}-{}".format('median', name)), image)
 
-    print('Median Method applied to {}.'. format(name))
+    count_blackpixels(image, name, 'Median')
 
 
 def get_section(image, shape):
@@ -169,37 +172,71 @@ def get_section(image, shape):
     return section
 
 
+def gen_histogram(image, name, path):
+
+    plt.hist(image.ravel(), bins=range(256), edgecolor='none', color="#3F5D7D")
+    plt.xlim([-1, 256])
+    plt.title(name.capitalize())
+
+    plt.savefig(os.path.join(path, 'output', 'histogram', name),
+                bbox_inches='tight')
+
+
+def count_blackpixels(image, name, method):
+
+    height, width = image.shape
+    count = 0
+
+    for y in range(0, height):
+        for x in range(0, width):
+            if image[y][x] == 0:
+                count = count + 1
+
+    print('{} Method applied to {} - {:.2f}% of black pixels.'. format(method,
+          name, 100 * count/(height*width)))
+
+
 def main():
 
-    qtd_images = 1 if len(sys.argv) == 1 else sys.argv[1]
-
+    print(sys.argv[1])
     images = []
     images_name = []
     path = load_images(images, images_name)
 
-    for image, image_name in zip(images[0:qtd_images], images_name[0:qtd_images]):
+    for i in range(0, len(images_name)):
+        images_name[i] = images_name[i][0:-4]
+        images_name[i] = images_name[i] + '.png'
+
+    qtd_images = 1 if len(sys.argv) == 1 else int(sys.argv[1])
+
+    images = images[0:qtd_images]
+
+    for image, image_name in zip(images, images_name):
         global_method(image, image_name, path)
 
-    for image, image_name in zip(images[0:qtd_images], images_name[0:qtd_images]):
+    for image, image_name in zip(images, images_name):
         bersen_method(image, image_name, path)
 
-    for image, image_name in zip(images[0:qtd_images], images_name[0:qtd_images]):
+    for image, image_name in zip(images, images_name):
         niblack_method(image, image_name, path)
 
-    for image, image_name in zip(images[0:qtd_images], images_name[0:qtd_images]):
+    for image, image_name in zip(images, images_name):
         sp_method(image, image_name, path)
 
-    for image, image_name in zip(images[0:qtd_images], images_name[0:qtd_images]):
+    for image, image_name in zip(images, images_name):
         phansalskar_method(image, image_name, path)
 
-    for image, image_name in zip(images[0:qtd_images], images_name[0:qtd_images]):
+    for image, image_name in zip(images, images_name):
         constrast_method(image, image_name, path)
 
-    for image, image_name in zip(images[0:qtd_images], images_name[0:qtd_images]):
+    for image, image_name in zip(images, images_name):
         mean_method(image, image_name, path)
 
-    for image, image_name in zip(images[0:qtd_images], images_name[0:qtd_images]):
+    for image, image_name in zip(images, images_name):
         median_method(image, image_name, path)
+
+    for image, image_name in zip(images, images_name):
+        gen_histogram(image, image_name, path)
 
 
 if __name__ == "__main__":
