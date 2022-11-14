e7569e9a863c3deb3eed92dc0cb04d53478e5b6e
Leonardo Rodrigues <lrm5060@gmail.com>
2022-11-14 11:41:37 -0300

    Add project 4

---
+++ MC920/Trabalho-4/load_images.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+
+
+def load_images(images, files):
+
+    # Get absolute path to images directory
+    local_path = os.path.abspath(os.getcwd())
+    absolute_path = os.path.join(local_path, "input")
+
+    # Create paths to images
+    images_paths = []
+    files.extend(os.listdir(absolute_path))
+
+    # For Apple macOS
+    if '.DS_Store' in files:
+        files.remove('.DS_Store')
+
+    for file in files:
+        images_paths.append(os.path.join(absolute_path, file))
+
+    # Read images from paths using cv2
+    for path in images_paths:
+        if "color" in path:
+            images.append(cv2.imread(path, cv2.IMREAD_COLOR))
+        else:
+            images.append(cv2.imread(path, cv2.IMREAD_GRAYSCALE))
+
+    # status to user
+    print('-- Images loaded!')
+
+    return local_path
